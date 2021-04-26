175095205c950605fecfb033aa55b1cddee553f8
Gibran Bacchus <gbacch@gmail.com>
2021-04-26 18:55:45 +0000

    mixed lists

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python4
 my_list = [ "192.168.0.5", 5060, "UP" ]
 print("The first item in the list (IP): " + my_list[0] )
 print("The second item in the list (port): " + str(my_list[1]) )
@@ -6,5 +6,10 @@ print("The last item in the list (state): " + my_list[2] )
 #Get ip, port, and state
 iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 # example 3 - use an 'f-string'
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# example 1 - add up the strings
 print(f"IP addresses: {iplist[3]}, and {iplist[4]}")
+#print("IP addresses:", iplist[3], ", and", iplist[4])
+#print("IP addresses: " + iplist[3] + ", and " + iplist[4]
 
