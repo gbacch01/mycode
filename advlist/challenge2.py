24ec93adb6677d2a23a623c97a9bc63b6bee5ac9
Gibran Bacchus <gbacch@gmail.com>
2021-04-26 21:10:40 +0000

    manipulating lists

---
+++ advlist/challenge2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+icecream= ["flavors", "salty"]
+num= [99]
+icecream.append(num)
+name = input("What is your name?")
+"{icecream[-1]} {icecreams[0]} and {name}
+print(icecream[-1] + "Flavors, and ", name + "chooses to be salty"
