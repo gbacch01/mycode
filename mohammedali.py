6181e835c523436ec60787dd55c46e345d1e918c
Gibran Bacchus <gbacch@gmail.com>
2021-04-29 19:07:15 +0000

    Learning to work with the matplotlib library

---
+++ mohammedali.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+## create a dictionary
+mali={"name":{"birth":"Cassius Clay","current":"Muhammad Ali"},"contact":{"phone":"333-444-5555","email":"thebest@boxing.com"},"favorites":{"number":56,"food":{"baked chicken":3.5,"mac and cheese":4.5,"spinach":2,"green peas":2},"drink":{"adult":["none"],"nonadult":["Mr. Champ soda"]}}}
+
+for x in mali["favorites"]["food"]:
+    print (x)
